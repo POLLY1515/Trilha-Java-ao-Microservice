6a6851f326bdb5a3ed292f33674ef05d852d3de9
POLLY1515 <amarantepolly2030@gmail.com>
2026-05-24 06:32:31 -0300

    Aula 3

---
+++ src/Exercicios/Aula3.java
@@ -0,0 +1,16 @@
+package Exercicios;
+
+public class Aula3 {
+
+	public static void main(String[] args) {
+
+		int idade = 3;
+		
+		if(idade >= 18) {
+		System.out.println("Acesso liberado!");
+		}else {
+			System.out.println("Acesso negado!");
+		}
+	}
+
+}

ba6ee622ae9568f21ffe025d023f3531932ab4df
POLLY1515 <amarantepolly2030@gmail.com>
2026-05-24 06:19:17 -0300

    Exercicio 1

---
+++ src/Exercicios/Aula1.java
@@ -0,0 +1,16 @@
+package Exercicios;
+
+public class Aula1 {
+
+	public static void main(String[] args) {
+		
+		String nome = "Poliana";
+		int idade = 33;
+		String linguagem = "Java";
+		
+		System.out.println("Meu nome é "+ nome +
+				", tenho "+ idade + "anos, e atualmente estou estudando a linguagem  " + linguagem);
+		
+	}
+
+}

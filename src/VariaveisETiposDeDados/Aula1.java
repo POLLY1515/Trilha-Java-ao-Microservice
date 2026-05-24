817fb21435c9925a11a7d3592c4b8c7bcbf6b117
POLLY1515 <amarantepolly2030@gmail.com>
2026-05-24 05:51:21 -0300

    Variaveis e tipos de dados

---
+++ src/VariaveisETiposDeDados/Aula1.java
@@ -0,0 +1,23 @@
+package VariaveisETiposDeDados;
+
+public class Aula1 {
+
+	public static void main(String[] args) {
+
+		int idade ; // declaracao
+		idade = 25; //atribuição
+		
+		String nome = "poliana"; //Inicialização
+		
+		//Tipos primitivos
+		
+		short ano = 2026; //Inteiros menores
+		int idadeA = 33;  //Inteiro em geral
+		long população = 900000L; //inteiros grandes
+		float nota = 9.7f; // decimal com menor precisao
+		double preco = 5.6; // decimal mais usado
+		char letra = 'a'; // um caracter
+		boolean ativo = false; // verdadeiro ou falso
+	}
+
+}

c30b229bf39006d7d78c4a17e32d3a69c0d62b5f
POLLY1515 <amarantepolly2030@gmail.com>
2026-05-24 05:55:02 -0300

    Condicionais

---
+++ src/Condicionais/Aula1.java
@@ -0,0 +1,33 @@
+package Condicionais;
+
+public class Aula1 {
+
+	public static void main(String[] args) {
+
+		//IF, executa um codigo somente se a condicao for verdadeira
+		
+		
+				int idade = 33;
+				if( idade == 33) {
+					
+					System.out.println(idade);
+				
+					}
+				
+			//IF e ELSE
+			//O else define o caminho alternativo quando a condicao do if nao é verdadeira.
+				
+				int notaFinal = 10;
+				
+				if(notaFinal < 100) {
+					
+					System.out.println("Reprovado");
+					
+					}else {
+					
+					System.out.println("Aprovado");
+					
+					}
+	}
+
+}

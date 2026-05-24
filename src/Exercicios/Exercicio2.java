ba2f6400d69ecd8c0b446d90770a8f136de5cc36
POLLY1515 <amarantepolly2030@gmail.com>
2026-05-24 06:30:04 -0300

    Exercicio 2

---
+++ src/Exercicios/Exercicio2.java
@@ -0,0 +1,16 @@
+package Exercicios;
+
+public class Exercicio2 {
+
+	public static void main(String[] args) {
+
+
+		Double produto = 549.90;
+		Double frete = 100.00;
+		
+		double valorTotal = produto + frete;
+		
+		System.out.println("Total: "+ valorTotal);
+	}
+
+}

6c1f7de1ba10da45df5f35daef2d2ae4eb8551f6
POLLY1515 <amarantepolly2030@gmail.com>
2026-05-24 06:34:25 -0300

    Exercicio 4

---
+++ src/Exercicios/Exercicio4.java
@@ -0,0 +1,16 @@
+package Exercicios;
+
+public class Exercicio4 {
+
+	public static void main(String[] args) {
+		
+		for( int i = 0; i<= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println("Numero par "+ i);
+			}
+			
+			
+		}
+	}
+
+}

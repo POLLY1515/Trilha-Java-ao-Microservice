e09b57fb017c4da6c6197558e628d1aa444b8cff
POLLY1515 <amarantepolly2030@gmail.com>
2026-05-29 09:10:41 -0300

    Listas

---
+++ src/Listas/Aula1.java
@@ -0,0 +1,22 @@
+package Listas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Aula1 {
+
+	public static void main(String[] args) {
+		
+		List<String> nomes = new ArrayList<>();
+		nomes.add("Poliana");
+		nomes.add("Tadeu");
+		nomes.add("Junior");
+		nomes.add("Jose");
+		
+		for (String nome : nomes) {
+			System.out.println("Nome: "+ nome);
+		}
+		
+	}
+
+}
